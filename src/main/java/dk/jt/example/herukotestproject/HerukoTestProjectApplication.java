bd31824fa8c95268bd57d188f343122845c8423c
jeppe-t <jeppethylstrup@yahoo.dk>
2021-10-08 15:46:39 +0200

    project added

---
+++ src/main/java/dk/jt/example/herukotestproject/HerukoTestProjectApplication.java
@@ -0,0 +1,13 @@
+package dk.jt.example.herukotestproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HerukoTestProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HerukoTestProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dk/jt/example/herukotestproject/HerukoTestProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package dk.jt.example.herukotestproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HerukoTestProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
